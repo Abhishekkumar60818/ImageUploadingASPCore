34181333b279df8de52e28b3d896a344d4d95303
Abhishek Singh <abhishekkumar60818@gmail.com>
2024-10-19 16:16:15 +0530

    Add project files.

---
+++ ImageUploadingASPCore/Models/ImageDbContext.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ImageUploadingASPCore.Models;
+
+public partial class ImageDbContext : DbContext
+{
+    public ImageDbContext()
+    {
+    }
+
+    public ImageDbContext(DbContextOptions<ImageDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        //if (!optionsBuilder.IsConfigured)
+        //{
+
+        //}
+
+
+
+    }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Product__3214EC072E885942");
+
+            entity.ToTable("Product");
+
+            entity.Property(e => e.ImagePath)
+                .HasMaxLength(500)
+                .IsUnicode(false)
+                .HasColumnName("image_path");
+            entity.Property(e => e.Name)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.Price).HasColumnName("price");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ ImageUploadingASPCore/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ImageUploadingASPCore.Models;
+
+public partial class Product
+{
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; } = null!;
+    [Required]
+    public int Price { get; set; }
+    [Required]
+    public string ImagePath { get; set; } = null!;
+}
